c1851944fb925f6fbfd00c6356509e69b0d1e48d
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-06-25 18:33:52 +0530

    Changes for the github

---
+++ Authentication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace Authentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "Admin,Editor")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin,Editor")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
